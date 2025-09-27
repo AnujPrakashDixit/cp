40d5198e2b755b24d6e7386f699486b2f5dca5a9
Anuj Prakash Dixit <anujprakashdixit@gmail.com>
2025-09-27 16:56:50 +0530

    till 4th problem(800)

--- 800/A_Cover_in_Water.cpp
+++ 800/A_Cover_in_Water.cpp
@@ -6,7 +6,38 @@ using namespace std;
 
 int main(){
 
+    int t;
+    cin>>t;
 
+    while(t--){
+        int n;
+        cin>>n;
+
+        string s;
+        cin>>s;
+
+        int maxConLen = 0;
+        int rnLen = 0;
+        int totalEmpcell = 0;
+        for(int i = 0 ; i < n; i++){
+            if(s[i] == '.'){
+                totalEmpcell++;
+                rnLen++;
+                maxConLen = max(maxConLen,rnLen);
+            }
+            else{
+                rnLen = 0;
+            }
+        }
+
+        if(maxConLen >= 3){
+            cout<<2<<endl;
+        }
+        else{
+            cout<<totalEmpcell<<endl;
+        }
+
+    }
 
     return 0;
 }
---
+++ 800/A_Game_with_Integers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        if (n % 3 == 0)
+        {
+            cout << "Second" << endl;
+        }
+        else if ((n % 3 == 1))
+        {
+            cout << "First" << endl;
+        }
+        else
+        {
+            cout << "First" << endl;
+        }
+    }
+}
